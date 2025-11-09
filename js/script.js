b675e263a3df2e0343ddfba574783bdf1cf84a74
Gabriel Trindade Dias <95586355+yGabrielT@users.noreply.github.com>
2025-11-09 16:31:27 -0300

    Principal Mudanças nos buttons e perguntas e sistema inicial

--- js/script.js
+++ js/script.js
@@ -1,1 +1,67 @@
+let questoesRepondidas = -1;
+let quantRespostasLayout = 4;
+const perguntas = [
+    "Pergunta1",
+    "Pergunta2",
+    "Pergunta3",
+    "Pergunta4"
+];
+const questoes = [
+    "Questao1",
+    "Questao2",
+    "Questao3",
+    "Questao4",
+    "Questao5",
+    "Questao6",
+    "Questao7",
+    "Questao8",
+    "Questao9",
+    "Questao10",
+    "Questao11",
+    "Questao12",
+    "Questao13",
+    "Questao14",
+    "Questao15",
+    "Questao16"
+];
 
+const respostas = []; 
+$(document).ready(function(){
+
+    IniciarQuestoes();
+}); 
+
+
+function IniciarQuestoes(){
+    AtualizarQuestoes();
+      $("#btn-0").click(function(){
+        AtualizarQuestoes();
+    });
+    $("#btn-1").click(function(){
+        AtualizarQuestoes();
+    });
+    $("#btn-2").click(function(){
+        AtualizarQuestoes();
+    });
+    $("#btn-3").click(function(){
+        AtualizarQuestoes();
+    });
+
+
+}
+
+function AtualizarQuestoes(){
+    questoesRepondidas++;
+    if (questoesRepondidas >= perguntas.length){
+        console.log('Todas questões respondidas');
+        return;
+    }
+    $(".pergunta-div").text(perguntas[questoesRepondidas])
+    let questoesRepondidasCount = (questoesRepondidas * quantRespostasLayout);
+    let questoesParaResponder = (questoesRepondidasCount + quantRespostasLayout)
+    let indexBtn = 0
+    for (let i = questoesRepondidasCount; i < questoesParaResponder; i++) {
+        $("#btn-" + indexBtn).text(questoes[i]);
+        indexBtn++;
+    }
+}
